7b69ff2ec843a745f8e738c340ad80dc143f3d8f
felipebmuniz <felipemuniz@alu.ufc.br>
2023-11-29 00:50:57 -0300

    fix: test user api refactor

--- src/pages/api/users.ts
+++ src/pages/api/users.ts
@@ -1,5 +1,5 @@
 import { NextApiHandler } from 'next';
-import axios, { AxiosResponse } from 'axios';
+import axios from 'axios';
 import { TypeMethod } from '@/types/forms';
 
 const fetchUsers: NextApiHandler = async function (request, response) {
@@ -8,38 +8,42 @@ const fetchUsers: NextApiHandler = async function (request, response) {
   const baseURL = process.env.NEXT_PUBLIC_API_URL;
   const url = baseURL + `Users/${query?.type ?? body?.type}`;
 
-  let apiResponse: AxiosResponse<any, any>;
-
   switch (method) {
     case TypeMethod.GET:
-      apiResponse = await axios.get(url, {
-        headers: { Authorization: headers.authorization },
-      });
-      if (apiResponse.status === 200) {
-        return response.status(200).json(apiResponse.data);
-      }
-
-      return response.status(400).json('Erro interno na API');
+      await axios
+        .get(url, {
+          headers: { Authorization: headers.authorization },
+        })
+        .then((res) => {
+          return response.status(200).json(res.data);
+        })
+        .catch((error) => {
+          return response.status(400 | 500).json(error.response.data);
+        });
 
     case TypeMethod.POST:
-      apiResponse = await axios.post(url, body?.data, {
-        headers: { Authorization: headers.authorization },
-      });
-      if (apiResponse.status === 200) {
-        return response.status(200).json(apiResponse.data);
-      }
-
-      return response.status(400).json('Erro interno na API');
+      await axios
+        .post(url, body?.data, {
+          headers: { Authorization: headers.authorization },
+        })
+        .then((res) => {
+          return response.status(200).json(res.data);
+        })
+        .catch((error) => {
+          return response.status(400 | 500).json(error.response.data);
+        });
 
     case TypeMethod.PUT:
-      apiResponse = await axios.put(url, body?.data, {
-        headers: { Authorization: headers.authorization },
-      });
-      if (apiResponse.status === 200) {
-        return response.status(200).json(apiResponse.data);
-      }
-
-      return response.status(400).json('Erro interno na API');
+      await axios
+        .put(url, body?.data, {
+          headers: { Authorization: headers.authorization },
+        })
+        .then((res) => {
+          return response.status(200).json(res.data);
+        })
+        .catch((error) => {
+          return response.status(400 | 500).json(error.response.data);
+        });
   }
 };
 
